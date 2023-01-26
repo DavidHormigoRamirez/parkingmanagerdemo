4894969fa3e679acc7df083ac6b7e963b44ce5a1
David Hormigo <dhorram948@g.educaand.es>
2023-01-26 19:09:22 +0100

    Bulma

--- src/main/java/com/park/tech/malaga/cpifp/parkingmanagerdemo/user/UserController.java
+++ src/main/java/com/park/tech/malaga/cpifp/parkingmanagerdemo/user/UserController.java
@@ -4,9 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
 @Controller
 public class UserController {
     
@@ -18,18 +15,5 @@ public class UserController {
         model.addAttribute("users", repository.getAll());
         return "userList";
 
-    }
-
-    @GetMapping("/newUser")
-    public String userForm(Model model) {
-        model.addAttribute("user", new User());
-        return "newUserForm";
-    }
-
-    @PostMapping("/newUser")
-    public String createUser(@ModelAttribute User user,Model model) {
-        model.addAttribute("user", user);
-        repository.add(user);
-        return "userList";
     }
 }
