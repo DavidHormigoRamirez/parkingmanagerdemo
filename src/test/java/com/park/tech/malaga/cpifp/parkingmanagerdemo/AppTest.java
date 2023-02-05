3c3246a182b28bc368fade50ae01fb2fc03e28f3
David Hormigo <dhorram948@g.educaand.es>
2023-02-05 15:21:13 +0100

    Añadir jacoco

---
+++ src/test/java/com/park/tech/malaga/cpifp/parkingmanagerdemo/AppTest.java
@@ -0,0 +1,13 @@
+package com.park.tech.malaga.cpifp.parkingmanagerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
