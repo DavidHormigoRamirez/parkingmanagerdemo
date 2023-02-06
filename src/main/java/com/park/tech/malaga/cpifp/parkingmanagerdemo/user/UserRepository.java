b7eb3de488de1e5482034241f32d99470f03eae8
David Hormigo Ramírez <dhorram948@g.educaand.es>
2023-02-06 16:03:10 +0100

    Cambios menores POM y Repository

--- src/main/java/com/park/tech/malaga/cpifp/parkingmanagerdemo/user/UserRepository.java
+++ src/main/java/com/park/tech/malaga/cpifp/parkingmanagerdemo/user/UserRepository.java
@@ -1,10 +1,7 @@
 package com.park.tech.malaga.cpifp.parkingmanagerdemo.user;
 
-
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends CrudRepository<User,Long> {
 
 }
